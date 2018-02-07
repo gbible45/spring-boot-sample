7d4838e3fdbb1fd625ac0d237efeacab6d5a6233
gbible <skyungkim@crossent.com>
2018-02-07 22:53:15 +0900

    commit

--- sample/src/main/java/com/sample/myApp/ApplicationConfig.java
+++ sample/src/main/java/com/sample/myApp/ApplicationConfig.java
@@ -1,5 +1,32 @@
 package com.sample.myApp;
 
+import java.nio.charset.Charset;
+
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
 public class ApplicationConfig {
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 
 }
--- sample/src/main/java/com/sample/myApp/framework/security/SecurityConfig.java
+++ sample/src/main/java/com/sample/myApp/framework/security/SecurityConfig.java
@@ -1,6 +1,64 @@
 package com.sample.myApp.framework.security;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableEncryptableProperties
+@EnableSpringDataWebSupport
+@EnableTransactionManagement
+@EnableJpaAuditing
+@EnableAsync
 public class SecurityConfig {
-	
-	
+
+	/**
+	 * For handling JSON vulnerability,
+	 * JSON response bodies would be prefixed with
+	 * this string.
+	 */
+	public final static String JSON_PREFIX = ")]}',\n";
+
+	private static final Log log = LogFactory.getLog(SecurityConfig.class);
+	/**
+	 * Prefixes JSON responses for JSON vulnerability. See for more details:
+	 * 
+	 * https://docs.angularjs.org/api/ng/service/$http
+	 * http://stackoverflow.com/questions/26384930/how-to-add-n-before-each-spring-json-response-to-prevent-common-vulnerab
+	 * 
+	 * To disable this, in your application.properties, use
+	 * lemon.enabled.json-prefix: false
+	 */
+	@Bean
+	@ConditionalOnProperty(name="spring.enabled.json-prefix", matchIfMissing=true)
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setJsonPrefix(JSON_PREFIX);
+
+        log.info("Configuring JSON vulnerability prefix ...");
+
+
+		ObjectMapper mapper = new ObjectMapper();
+		Hibernate4Module hm = new Hibernate4Module();
+		hm.disable(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION);  // @Transient 변환을 허용
+		mapper.registerModule(hm);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		converter.setObjectMapper(mapper);
+
+		log.info("Configuring JSON hibernate lazy objects ...");
+
+        return converter;
+	}
 }
